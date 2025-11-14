252e469611617834dc2aa22b656f212b13288e38
Oleksandr (Sashko) Mediantsev <zencrazycat@gmail.com>
2025-11-14 17:22:09 +0200

    Update collier transform scripts

--- collier/scripts/data_extractor.js
+++ collier/scripts/data_extractor.js
@@ -782,8 +782,8 @@ function main() {
 
     const relDf = {
       type: "deed_has_file",
-      from: `./deed_${idx + 1}.json`,
-      to: `./file_${idx + 1}.json`,
+      from: { "/": `./deed_${idx + 1}.json` },
+      to: { "/": `./file_${idx + 1}.json` },
     };
     fs.writeFileSync(
       path.join(dataDir, `relationship_deed_file_${idx + 1}.json`),
@@ -818,8 +818,8 @@ function main() {
     if (deedIdx > 0) {
       const rel = {
         type: "sales_history_has_deed",
-        from: `./sales_${idx + 1}.json`,
-        to: `./deed_${deedIdx}.json`,
+        from: { "/": `./sales_${idx + 1}.json` },
+        to: { "/": `./deed_${deedIdx}.json` },
       };
       fs.writeFileSync(
         path.join(dataDir, `relationship_sales_deed_${idx + 1}.json`),
