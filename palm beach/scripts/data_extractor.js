022e805ba95335557cab0cce6be21d38aac0fc6d
Mykyta Ovsiienko <68018445+movsiienko@users.noreply.github.com>
2025-11-17 05:03:13 +0200

    Update palm beach transform scripts

--- palm beach/scripts/data_extractor.js
+++ palm beach/scripts/data_extractor.js
@@ -1670,7 +1670,13 @@ const NORMALIZED_ADDRESS_SCHEMA_TEMPLATE = Object.freeze(
   }, {}),
 );
 
-const RAW_ADDRESS_EXCLUDED_FIELDS = new Set();
+const RAW_ADDRESS_EXCLUDED_FIELDS = new Set([
+  "street_number",
+  "street_name",
+  "street_pre_directional_text",
+  "street_post_directional_text",
+  "street_suffix_type",
+]);
 
 const RAW_ADDRESS_ALLOWED_FIELDS = NORMALIZED_ADDRESS_FIELDS.filter(
   (field) => !RAW_ADDRESS_EXCLUDED_FIELDS.has(field),
