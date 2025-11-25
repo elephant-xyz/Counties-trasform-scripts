a0e263ca597c0bce36abe6c280c92f96a7725c8b
Oleksandr (Sashko) Mediantsev <zencrazycat@gmail.com>
2025-11-26 01:43:23 +0200

    Update gulf transform scripts

--- gulf/scripts/data_extractor.js
+++ gulf/scripts/data_extractor.js
@@ -1157,6 +1157,12 @@ function writeProperty($, parcelId) {
   const years = extractBuildingYears($);
   const totalArea = extractAreas($);
 
+  // Create parcel.json
+  const parcel = {
+    parcel_identifier: parcelId || "",
+  };
+  writeJSON(path.join("data", "parcel.json"), parcel);
+
   const property = {
     parcel_identifier: parcelId || "",
     property_legal_description_text: legal || null,
