d26053e29a8bf71c38283c5862c38a82a19b9538
Oleksandr (Sashko) Mediantsev <zencrazycat@gmail.com>
2025-11-26 04:26:21 +0200

    Update putnam transform scripts

--- putnam/scripts/structureMapping.js
+++ putnam/scripts/structureMapping.js
@@ -293,7 +293,7 @@ function parseFloorFinish(value) {
     "17 - Rough Pine": null,
     "20 - Earth": null,
     "08 - Carpeting 2": "Carpet",
-    "01 - Terrazzo Mono": "Terazzo",
+    "01 - Terrazzo Mono": "Terrazzo",
     "18 - Roll Congoleum": null,
   }
   if (value in valueMapping) {
