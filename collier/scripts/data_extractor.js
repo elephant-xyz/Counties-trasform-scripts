d3b4fa07cebdaf069285452ed04b1e5911f4b0a7
Oleksandr (Sashko) Mediantsev <zencrazycat@gmail.com>
2025-12-05 13:08:44 +0200

    Update collier transform scripts

--- collier/scripts/data_extractor.js
+++ collier/scripts/data_extractor.js
@@ -937,6 +937,19 @@ function main() {
       path.join(dataDir, "utility.json"),
       JSON.stringify(utilsEntry, null, 2),
     );
+
+    // Create relationship from property to utility
+    fs.writeFileSync(
+      path.join(dataDir, "relationship_property_has_utility_1.json"),
+      JSON.stringify(
+        {
+          from: { "/": "./property.json" },
+          to: { "/": "./utility.json" },
+        },
+        null,
+        2,
+      ),
+    );
   }
 
   // Layouts from owners/layout_data.json
