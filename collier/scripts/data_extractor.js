5e7c7f8dc77b4615ddf8422cfc160e50921211cc
Oleksandr (Sashko) Mediantsev <zencrazycat@gmail.com>
2025-12-10 14:58:56 +0200

    Update collier transform scripts

--- collier/scripts/data_extractor.js
+++ collier/scripts/data_extractor.js
@@ -1058,8 +1058,7 @@ function main() {
         visible_damage: null,
         window_design_type: null,
         window_material_type: null,
-        window_treatment_type: 123456789,
-        non_existing_field: null,
+        window_treatment_type: null,
         ...customFields, // Override with specific values
       };
     };
