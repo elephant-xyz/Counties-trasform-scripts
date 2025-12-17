8075e4799be7c604aec3aadaa58314b742b11333
Mykyta Ovsiienko <68018445+movsiienko@users.noreply.github.com>
2025-12-17 02:40:13 +0200

    Update miami dade transform scripts

--- miami dade/scripts/data_extractor.js
+++ miami dade/scripts/data_extractor.js
@@ -2462,8 +2462,8 @@ function main() {
     let relSDIdx = 1;
     for (const [sIndex, dIndex] of deedMap.entries()) {
       const relSD = {
-        from: { "/": `./deed_${dIndex}.json` },
-        to: { "/": `./sales_history_${sIndex}.json` },
+        from: { "/": `./sales_history_${sIndex}.json` },
+        to: { "/": `./deed_${dIndex}.json` },
       };
       writeJson(path.join("data", `relationship_sales_history_${relSDIdx}_has_deed.json`), relSD);
       relSDIdx++;
